97b8fa34bb1b53fd5c9f297a145e71f60bd444b2
Ryczko <Ryczko@users.noreply.github.com>
2020-04-23 18:45:45 +0200

    Updates

---
+++ precache-manifest.37d9096f6fdcc8b19c534731216977b8.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f76f89f0d2445d969dc06723bf7e1a4c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "3deb5634e4aefe893721",
+    "url": "/Portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "686d01a0ab05980235b1",
+    "url": "/Portfolio/static/css/main.3147ccb2.chunk.css"
+  },
+  {
+    "revision": "3deb5634e4aefe893721",
+    "url": "/Portfolio/static/js/2.cee9d276.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/Portfolio/static/js/2.cee9d276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686d01a0ab05980235b1",
+    "url": "/Portfolio/static/js/main.3a77df11.chunk.js"
+  },
+  {
+    "revision": "9da7e9aca51c2cf80e94",
+    "url": "/Portfolio/static/js/runtime-main.5677dc13.js"
+  },
+  {
+    "revision": "f8fd47af399c7d0393604853c15816ba",
+    "url": "/Portfolio/static/media/calculator.f8fd47af.png"
+  },
+  {
+    "revision": "16bf79f75a63d3fb9f782e54582d69f2",
+    "url": "/Portfolio/static/media/corona.16bf79f7.png"
+  },
+  {
+    "revision": "60384a904d57c32996a6f75298a352c8",
+    "url": "/Portfolio/static/media/fontello.60384a90.woff2"
+  },
+  {
+    "revision": "adec52d1ca7564a0d83c769f26c1a09f",
+    "url": "/Portfolio/static/media/fontello.adec52d1.eot"
+  },
+  {
+    "revision": "b11d21181556797e84e89b3123d9a208",
+    "url": "/Portfolio/static/media/fontello.b11d2118.woff"
+  },
+  {
+    "revision": "b1cbfb326f143103cddafc702115d598",
+    "url": "/Portfolio/static/media/fontello.b1cbfb32.ttf"
+  },
+  {
+    "revision": "d16f32d3733937e3d3f850cc90cc51e2",
+    "url": "/Portfolio/static/media/fontello.d16f32d3.svg"
+  },
+  {
+    "revision": "da07d96e4f010670f59b5fbc343c5f8e",
+    "url": "/Portfolio/static/media/git.da07d96e.png"
+  },
+  {
+    "revision": "311d59a66f40f52d1584ad3d6033e6bb",
+    "url": "/Portfolio/static/media/gsap.311d59a6.svg"
+  },
+  {
+    "revision": "f4a7a529c2d7feee02dfaf81dab00bf1",
+    "url": "/Portfolio/static/media/laptop.f4a7a529.svg"
+  },
+  {
+    "revision": "0239bd87b0e44bbed4f41976c5f21a7d",
+    "url": "/Portfolio/static/media/mail.0239bd87.png"
+  },
+  {
+    "revision": "8c8dcf0da3d1579ffe767dc718b0f330",
+    "url": "/Portfolio/static/media/poland-flag.8c8dcf0d.png"
+  },
+  {
+    "revision": "ac9cb47b67d70dc44a4aee22a0b09871",
+    "url": "/Portfolio/static/media/sass.ac9cb47b.png"
+  },
+  {
+    "revision": "14e95c8f04cfee6512bd4f884efbb9e5",
+    "url": "/Portfolio/static/media/shadow.14e95c8f.png"
+  },
+  {
+    "revision": "288caf89d9e32474055cc737fe86f763",
+    "url": "/Portfolio/static/media/socket.288caf89.png"
+  },
+  {
+    "revision": "327e05a0f8f9f86b13dd4611b60ddd0a",
+    "url": "/Portfolio/static/media/uk-flag.327e05a0.png"
+  },
+  {
+    "revision": "f7206caee771a66fff31697ff89e5b8c",
+    "url": "/Portfolio/static/media/wave-footer.f7206cae.svg"
+  }
+]);
